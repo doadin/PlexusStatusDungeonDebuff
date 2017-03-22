4adc99da0d2595496ef57d3c0cf606d84a5a5e91
Doadin <tbkizle@gmail.com>
2017-03-22 19:06:05 -0400

    Update Vault of the Wardens
    Vault of the Wardens
      Trash
        Gift of the Doomsayer
        Barbed Web
        A Mother's Love
        Torment
        Double Strike
        Nightmares
        Corrupted Touch
        Foul Stench
        Drain
        Razors
        Anguished Souls
      Glazer
        Beam
        Lingering Gaze
        Fired Up
      Ash'Golm
        Lava
      Cordana Felsong
        Deepening Shadows

--- Legion/VaultoftheWardens.lua
+++ Legion/VaultoftheWardens.lua
@@ -12,22 +12,39 @@ end
 --Whole Dungeon/Trash/Mythic Plus
 GridStatusRaidDebuff:DebuffId(zoneid, 209858, 1, 6, 6) --Necrotic Rot
 GridStatusRaidDebuff:DebuffId(zoneid, 159172, 1, 6, 6) --Overflow
+GridStatusRaidDebuff:DebuffId(zoneid, 193164, 1, 6, 6) --Gift of the Doomsayer
+GridStatusRaidDebuff:DebuffId(zoneid, 193956, 1, 6, 6) --Barbed Web
+GridStatusRaidDebuff:DebuffId(zoneid, 194064, 1, 6, 6) --A Mother's Love
+GridStatusRaidDebuff:DebuffId(zoneid, 202615, 1, 6, 6) --Torment
+GridStatusRaidDebuff:DebuffId(zoneid, 193607, 1, 6, 6) --Double Strike
+GridStatusRaidDebuff:DebuffId(zoneid, 193069, 1, 6, 6) --Nightmares
+GridStatusRaidDebuff:DebuffId(zoneid, 206019, 1, 6, 6) --Corrupted Touch
+GridStatusRaidDebuff:DebuffId(zoneid, 210202, 1, 6, 6) --Foul Stench
+GridStatusRaidDebuff:DebuffId(zoneid, 202658, 1, 6, 6) --Drain
+GridStatusRaidDebuff:DebuffId(zoneid, 193969, 1, 6, 6) --Razors
+GridStatusRaidDebuff:DebuffId(zoneid, 202608, 1, 6, 6) --Anguished Souls
 
---Vigilant Kaathar
---GridStatusRaidDebuff:BossNameId(zoneid, 10, "Vigilant Kaathar")
+--Tirathon Saltheril
+--GridStatusRaidDebuff:BossNameId(zoneid, 10, "Tirathon Saltheril")
 --GridStatusRaidDebuff:DebuffId(zoneid, 153480, 11, 6, 6) 
 
 
---Soulbinder Nyami
---GridStatusRaidDebuff:BossNameId(zoneid, 20, "Soulbinder Nyami")
+--Inquisitor Tormentorum
+--GridStatusRaidDebuff:BossNameId(zoneid, 20, "Inquisitor Tormentorum")
 --GridStatusRaidDebuff:DebuffId(zoneid, 154415, 21, 6, 6) 
 
 
---Azzakel
---GridStatusRaidDebuff:BossNameId(zoneid, 30, "Azzakel")
---GridStatusRaidDebuff:DebuffId(zoneid, 153234, 31, 6, 6)
+--Glazer
+GridStatusRaidDebuff:BossNameId(zoneid, 30, "Glazer")
+GridStatusRaidDebuff:DebuffId(zoneid, 202046, 31, 6, 6) --Beam
+GridStatusRaidDebuff:DebuffId(zoneid, 194942, 32, 6, 6) --Lingering Gaze
+GridStatusRaidDebuff:DebuffId(zoneid, 215478, 33, 6, 6) --Fired Up
 
 
---Teron'gor
---GridStatusRaidDebuff:BossNameId(zoneid, 40, "Teron'gor")
---GridStatusRaidDebuff:DebuffId(zoneid, 156829, 41, 6, 6)
+--Ash'Golm
+GridStatusRaidDebuff:BossNameId(zoneid, 40, "Ash'Golm")
+GridStatusRaidDebuff:DebuffId(zoneid, 192519, 41, 6, 6) --Lava
+
+--Cordana Felsong
+GridStatusRaidDebuff:BossNameId(zoneid, 50, "Cordana Felsong")
+GridStatusRaidDebuff:DebuffId(zoneid, 213395, 41, 6, 6) --Deepening Shadows
