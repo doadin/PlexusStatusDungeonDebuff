302345f2edaf2454b22cc9d5dcc3c630f98d05fa
Doadin <tbkizle@gmail.com>
2017-03-22 19:20:01 -0400

    Update Black Rook Hold
    Black Rook Hold
      Trash
             Necrotic Rot
        Overflow
        Bloodthirsty Leap
        Soul Venom
        Raven's Dive
        Bonebreaking Strike
        Soul Blade
      Illysanna Ravencrest
        Brutal Glaive
      Smashspite the Hateful 
        Hateful Charge 
        Brutal Haymaker 
      Lord Kur'talos Ravencrest
        Unerring Shear 
      Latosius
        Stinging Swarm

--- Legion/BlackRookHold.lua
+++ Legion/BlackRookHold.lua
@@ -12,22 +12,32 @@ end
 --Whole Dungeon/Trash/Mythic Plus
 GridStatusRaidDebuff:DebuffId(zoneid, 209858, 1, 6, 6) --Necrotic Rot
 GridStatusRaidDebuff:DebuffId(zoneid, 159172, 1, 6, 6) --Overflow
+GridStatusRaidDebuff:DebuffId(zoneid, 225963, 1, 6, 6) --Bloodthirsty Leap
+GridStatusRaidDebuff:DebuffId(zoneid, 225909, 1, 6, 6) --Soul Venom
+GridStatusRaidDebuff:DebuffId(zoneid, 214002, 1, 6, 6) --Raven's Dive
+GridStatusRaidDebuff:DebuffId(zoneid, 200261, 1, 6, 6) --Bonebreaking Strike
+GridStatusRaidDebuff:DebuffId(zoneid, 200084, 1, 6, 6) --Soul Blade
 
---Vigilant Kaathar
---GridStatusRaidDebuff:BossNameId(zoneid, 10, "Vigilant Kaathar")
+--Amalgam of Souls
+--GridStatusRaidDebuff:BossNameId(zoneid, 10, "Amalgam of Souls")
 --GridStatusRaidDebuff:DebuffId(zoneid, 153480, 11, 6, 6) 
 
 
---Soulbinder Nyami
---GridStatusRaidDebuff:BossNameId(zoneid, 20, "Soulbinder Nyami")
---GridStatusRaidDebuff:DebuffId(zoneid, 154415, 21, 6, 6) 
+--Illysanna Ravencrest
+GridStatusRaidDebuff:BossNameId(zoneid, 20, "Illysanna Ravencrest")
+GridStatusRaidDebuff:DebuffId(zoneid, 197546, 21, 6, 6) --Brutal Glaive
 
 
---Azzakel
---GridStatusRaidDebuff:BossNameId(zoneid, 30, "Azzakel")
---GridStatusRaidDebuff:DebuffId(zoneid, 153234, 31, 6, 6)
+--Smashspite the Hateful
+GridStatusRaidDebuff:BossNameId(zoneid, 30, "Smashspite the Hateful")
+GridStatusRaidDebuff:DebuffId(zoneid, 224188, 31, 6, 6) --Hateful Charge
+GridStatusRaidDebuff:DebuffId(zoneid, 198245, 32, 6, 6) --Brutal Haymaker
 
 
---Teron'gor
---GridStatusRaidDebuff:BossNameId(zoneid, 40, "Teron'gor")
---GridStatusRaidDebuff:DebuffId(zoneid, 156829, 41, 6, 6)
+--Lord Kur'talos Ravencrest
+GridStatusRaidDebuff:BossNameId(zoneid, 40, "Lord Kur'talos Ravencrest")
+GridStatusRaidDebuff:DebuffId(zoneid, 198635, 41, 6, 6) --Unerring Shear
+
+--Latosius
+GridStatusRaidDebuff:BossNameId(zoneid, 50, "Latosius")
+GridStatusRaidDebuff:DebuffId(zoneid, 201733, 51, 6, 6) --Stinging Swarm
