fdd0414ded7acf6173ffb8cd1c8b0404bc2260cb
Doadin <tbkizle@gmail.com>
2017-03-28 15:13:49 -0400

    Update Neltharion's Lair
    Trash
      Necrotic Rot
      Overflow
      Burning Hatred
      Leech
      Choking Dust
      Bone Chomp
      Rancid Maw
      Impaling Shard
      Viscid Bile
      Stone Gaze
      Bound
      Rancid Ooze
    Rokmora
      Crystalline Ground
    Naraxas
      Toxic Retch
      Spiked Tongue

--- Legion/Neltharion'sLair.lua
+++ Legion/Neltharion'sLair.lua
@@ -12,22 +12,35 @@ end
 --Whole Dungeon/Trash/Mythic Plus
 GridStatusRaidDebuff:DebuffId(zoneid, 209858, 1, 6, 6) --Necrotic Rot
 GridStatusRaidDebuff:DebuffId(zoneid, 159172, 1, 6, 6) --Overflow
+GridStatusRaidDebuff:DebuffId(zoneid, 200154, 1, 6, 6) --Burning Hatred
+GridStatusRaidDebuff:DebuffId(zoneid, 202231, 1, 6, 6) --Leech
+GridStatusRaidDebuff:DebuffId(zoneid, 192800, 1, 6, 6) --Choking Dust
+GridStatusRaidDebuff:DebuffId(zoneid, 193639, 1, 6, 6) --Bone Chomp
+GridStatusRaidDebuff:DebuffId(zoneid, 188494, 1, 6, 6) --Rancid Maw
+GridStatusRaidDebuff:DebuffId(zoneid, 193941, 1, 6, 6) --Impaling Shard
+GridStatusRaidDebuff:DebuffId(zoneid, 183465, 1, 6, 6) --Viscid Bile
+GridStatusRaidDebuff:DebuffId(zoneid, 202181, 1, 6, 6) --Stone Gaze
+GridStatusRaidDebuff:DebuffId(zoneid, 193585, 1, 6, 6) --Bound
+GridStatusRaidDebuff:DebuffId(zoneid, 226388, 1, 6, 6) --Rancid Ooze
 
---Vigilant Kaathar
---GridStatusRaidDebuff:BossNameId(zoneid, 10, "Vigilant Kaathar")
---GridStatusRaidDebuff:DebuffId(zoneid, 153480, 11, 6, 6) 
+--Rokmora
+GridStatusRaidDebuff:BossNameId(zoneid, 10, "Rokmora")
+GridStatusRaidDebuff:DebuffId(zoneid, 215898, 11, 6, 6) --Crystalline Ground
 
 
---Soulbinder Nyami
---GridStatusRaidDebuff:BossNameId(zoneid, 20, "Soulbinder Nyami")
---GridStatusRaidDebuff:DebuffId(zoneid, 154415, 21, 6, 6) 
+
+--Ularogg Cragshaper
+--GridStatusRaidDebuff:BossNameId(zoneid, 20, "Ularogg Cragshaper")
+--GridStatusRaidDebuff:DebuffId(zoneid, 196562, 21, 6, 6) --Volatile Magic
 
 
---Azzakel
---GridStatusRaidDebuff:BossNameId(zoneid, 30, "Azzakel")
---GridStatusRaidDebuff:DebuffId(zoneid, 153234, 31, 6, 6)
+--Naraxas
+GridStatusRaidDebuff:BossNameId(zoneid, 30, "Naraxas")
+GridStatusRaidDebuff:DebuffId(zoneid, 217851, 31, 6, 6) --Toxic Retch
+GridStatusRaidDebuff:DebuffId(zoneid, 199178, 32, 6, 6) --Spiked Tongue
 
 
---Teron'gor
---GridStatusRaidDebuff:BossNameId(zoneid, 40, "Teron'gor")
---GridStatusRaidDebuff:DebuffId(zoneid, 156829, 41, 6, 6)
+--Dargrul
+--GridStatusRaidDebuff:BossNameId(zoneid, 40, "Dargrul")
+--GridStatusRaidDebuff:DebuffId(zoneid, 200284, 41, 6, 6) --Tangled Web
+
