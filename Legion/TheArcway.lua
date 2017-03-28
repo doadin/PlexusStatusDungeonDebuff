1e79f7013d0b685ea420221467e51026e42570eb
Doadin <tbkizle@gmail.com>
2017-03-28 15:03:18 -0400

    Update The Arcway
    Trash
      Arcanic Bane
      Searing Wound
      Torment
      Parallel Dimension
      Nether Wound
      Devour
      Corrosion
      Collapsing Rift
      Siphon Essence
      Black Plague
      Consume
      Ooze Puddle
    Corstilax
      Suppression Protocol
      Quarantine
      Destabilized Orb
    Ivanyr
      Volatile Magic
    Nal'tira
      Tangled Web
      Nether Venom
    Advisor Vandros
      Banished In Time
      Force Nova

--- Legion/TheArcway.lua
+++ Legion/TheArcway.lua
@@ -12,22 +12,42 @@ end
 --Whole Dungeon/Trash/Mythic Plus
 GridStatusRaidDebuff:DebuffId(zoneid, 209858, 1, 6, 6) --Necrotic Rot
 GridStatusRaidDebuff:DebuffId(zoneid, 159172, 1, 6, 6) --Overflow
+GridStatusRaidDebuff:DebuffId(zoneid, 210645, 1, 6, 6) --Arcanic Bane
+GridStatusRaidDebuff:DebuffId(zoneid, 211756, 1, 6, 6) --Searing Wound
+GridStatusRaidDebuff:DebuffId(zoneid, 226269, 1, 6, 6) --Torment
+GridStatusRaidDebuff:DebuffId(zoneid, 211125, 1, 6, 6) --Parallel Dimension
+GridStatusRaidDebuff:DebuffId(zoneid, 211000, 1, 6, 6) --Nether Wound
+GridStatusRaidDebuff:DebuffId(zoneid, 211543, 1, 6, 6) --Devour
+GridStatusRaidDebuff:DebuffId(zoneid, 202156, 1, 6, 6) --Corrosion
+GridStatusRaidDebuff:DebuffId(zoneid, 210750, 1, 6, 6) --Collapsing Rift
+GridStatusRaidDebuff:DebuffId(zoneid, 210684, 1, 6, 6) --Siphon Essence
+GridStatusRaidDebuff:DebuffId(zoneid, 203592, 1, 6, 6) --Black Plague
+GridStatusRaidDebuff:DebuffId(zoneid, 202232, 1, 6, 6) --Consume
+GridStatusRaidDebuff:DebuffId(zoneid, 194006, 1, 6, 6) --Ooze Puddle
 
---Vigilant Kaathar
---GridStatusRaidDebuff:BossNameId(zoneid, 10, "Vigilant Kaathar")
---GridStatusRaidDebuff:DebuffId(zoneid, 153480, 11, 6, 6) 
+--Corstilax
+GridStatusRaidDebuff:BossNameId(zoneid, 10, "Corstilax")
+GridStatusRaidDebuff:DebuffId(zoneid, 196074, 11, 6, 6) --Suppression Protocol
+GridStatusRaidDebuff:DebuffId(zoneid, 195804, 12, 6, 6) --Quarantine
+GridStatusRaidDebuff:DebuffId(zoneid, 220500, 13, 6, 6) --Destabilized Orb
 
 
---Soulbinder Nyami
---GridStatusRaidDebuff:BossNameId(zoneid, 20, "Soulbinder Nyami")
---GridStatusRaidDebuff:DebuffId(zoneid, 154415, 21, 6, 6) 
+--Ivanyr
+GridStatusRaidDebuff:BossNameId(zoneid, 20, "Ivanyr")
+GridStatusRaidDebuff:DebuffId(zoneid, 196562, 21, 6, 6) --Volatile Magic
 
 
---Azzakel
---GridStatusRaidDebuff:BossNameId(zoneid, 30, "Azzakel")
+--General Xakal
+--GridStatusRaidDebuff:BossNameId(zoneid, 30, "General Xakal")
 --GridStatusRaidDebuff:DebuffId(zoneid, 153234, 31, 6, 6)
 
 
---Teron'gor
---GridStatusRaidDebuff:BossNameId(zoneid, 40, "Teron'gor")
---GridStatusRaidDebuff:DebuffId(zoneid, 156829, 41, 6, 6)
+--Nal'tira
+GridStatusRaidDebuff:BossNameId(zoneid, 40, "Nal'tira")
+GridStatusRaidDebuff:DebuffId(zoneid, 200284, 41, 6, 6) --Tangled Web
+GridStatusRaidDebuff:DebuffId(zoneid, 200040, 42, 6, 6) --Nether Venom
+
+--Advisor Vandros
+GridStatusRaidDebuff:BossNameId(zoneid, 50, "Advisor Vandros")
+GridStatusRaidDebuff:DebuffId(zoneid, 203914, 51, 6, 6) --Banished In Time
+GridStatusRaidDebuff:DebuffId(zoneid, 203139, 52, 6, 6) --Force Nova
