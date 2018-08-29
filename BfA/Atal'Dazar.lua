c654202f128098f6d1adebec807a7a00ee6e05b3
Unknown <tbkizle@gmail.com>
2018-08-29 15:34:45 -0400

    Update Atal'Dazar

--- BfA/Atal'Dazar.lua
+++ BfA/Atal'Dazar.lua
@@ -1,6 +1,29 @@
 --local zone = "Atal'Dazar"
 local zoneid = 934
 
+GridStatusRaidDebuff:DebuffId(zoneid, 254959, 1, 6, 6, true) --Soulburn           
+GridStatusRaidDebuff:DebuffId(zoneid, 250372, 1, 6, 6, true) --Lingering Nausea   
+GridStatusRaidDebuff:DebuffId(zoneid, 250096, 1, 6, 6, true) --Wracking Pain      
+GridStatusRaidDebuff:DebuffId(zoneid, 250036, 1, 6, 6, true) --Shadowy Remains    
+GridStatusRaidDebuff:DebuffId(zoneid, 257692, 1, 6, 6, true) --Tiki Blaze         
+GridStatusRaidDebuff:DebuffId(zoneid, 257483, 1, 6, 6, true) --Pile of Bones      
+GridStatusRaidDebuff:DebuffId(zoneid, 249919, 1, 6, 6, true) --Skewer             
+GridStatusRaidDebuff:DebuffId(zoneid, 253562, 1, 6, 6, true) --Wildfire           
+GridStatusRaidDebuff:DebuffId(zoneid, 253279, 1, 6, 6, true) --Merciless Assault  
+GridStatusRaidDebuff:DebuffId(zoneid, 272631, 1, 6, 6, true) --Battering Maul     
+GridStatusRaidDebuff:DebuffId(zoneid, 256138, 1, 6, 6, true) --Fervent Strike     
+GridStatusRaidDebuff:DebuffId(zoneid, 255581, 1, 6, 6, true) --Gilded Claws       
+GridStatusRaidDebuff:DebuffId(zoneid, 279118, 1, 6, 6, true) --Unstable Hex       
+GridStatusRaidDebuff:DebuffId(zoneid, 273185, 1, 6, 6, true) --Shield Bash        
+GridStatusRaidDebuff:DebuffId(zoneid, 255620, 1, 6, 6, true) --Festering Eruption 
+GridStatusRaidDebuff:DebuffId(zoneid, 251187, 1, 6, 6, true) --Leaping Thrash     
+GridStatusRaidDebuff:DebuffId(zoneid, 255895, 1, 6, 6, true) --Poisoned Claws     
+GridStatusRaidDebuff:DebuffId(zoneid, 256959, 1, 6, 6, true) --Rotting Decay      
+GridStatusRaidDebuff:DebuffId(zoneid, 252923, 1, 6, 6, true) --Venom Blast        
+GridStatusRaidDebuff:DebuffId(zoneid, 252687, 1, 6, 6, true) --Venomfang Strike   
+GridStatusRaidDebuff:DebuffId(zoneid, 252666, 1, 6, 6, true) --Venom-Tipped Blade 
+GridStatusRaidDebuff:DebuffId(zoneid, 255041, 1, 6, 6, true) --Terrifying Screech 
+
 --Priestess Alun'az
 GridStatusRaidDebuff:BossNameId(zoneid, 10, "Priestess Alun'az")
 GridStatusRaidDebuff:DebuffId(zoneid, 255582, 11, 6, 6, true) --Molten Gold
