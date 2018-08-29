10048eb71b208c03ed101820e8119a54c3074054
Unknown <tbkizle@gmail.com>
2018-08-29 17:30:18 -0400

    Update Temple of Sethraliss

--- BfA/Temple of Sethraliss.lua
+++ BfA/Temple of Sethraliss.lua
@@ -1,6 +1,29 @@
 --local zone = "Temple of Sethraliss"
 local zoneid = 1038
 
+GridStatusRaidDebuff:DebuffId(zoneid, 272657, 1, 6, 6, true) --Noxious Breath        
+GridStatusRaidDebuff:DebuffId(zoneid, 273274, 1, 6, 6, true) --Polarized Field       
+GridStatusRaidDebuff:DebuffId(zoneid, 268024, 1, 6, 6, true) --Pulse                 
+GridStatusRaidDebuff:DebuffId(zoneid, 264574, 1, 6, 6, true) --Power Shot            
+GridStatusRaidDebuff:DebuffId(zoneid, 272670, 1, 6, 6, true) --Shoot                 
+GridStatusRaidDebuff:DebuffId(zoneid, 262046, 1, 6, 6, true) --Krolusk Bite          
+GridStatusRaidDebuff:DebuffId(zoneid, 269670, 1, 6, 6, true) --Empowerment           
+GridStatusRaidDebuff:DebuffId(zoneid, 279014, 1, 6, 6, true) --Cardiac Shock         
+GridStatusRaidDebuff:DebuffId(zoneid, 268061, 1, 6, 6, true) --Chain Lightning       
+GridStatusRaidDebuff:DebuffId(zoneid, 263573, 1, 6, 6, true) --Cyclone Strike        
+GridStatusRaidDebuff:DebuffId(zoneid, 264587, 1, 6, 6, true) --Head Slam             
+GridStatusRaidDebuff:DebuffId(zoneid, 263927, 1, 6, 6, true) --Toxic Pool            
+GridStatusRaidDebuff:DebuffId(zoneid, 264206, 1, 6, 6, true) --Burrow                
+GridStatusRaidDebuff:DebuffId(zoneid, 268703, 1, 6, 6, true) --Lightning Bolt        
+GridStatusRaidDebuff:DebuffId(zoneid, 272699, 1, 6, 6, true) --Venomous Spit         
+GridStatusRaidDebuff:DebuffId(zoneid, 272696, 1, 6, 6, true) --Lightning in a Bottle 
+GridStatusRaidDebuff:DebuffId(zoneid, 267278, 1, 6, 6, true) --Electrocute           
+GridStatusRaidDebuff:DebuffId(zoneid, 272654, 1, 6, 6, true) --Head Butt             
+GridStatusRaidDebuff:DebuffId(zoneid, 268007, 1, 6, 6, true) --Heart Attack          
+GridStatusRaidDebuff:DebuffId(zoneid, 272655, 1, 6, 6, true) --Scouring Sand         
+GridStatusRaidDebuff:DebuffId(zoneid, 268993, 1, 6, 6, true) --Cheap Shot            
+GridStatusRaidDebuff:DebuffId(zoneid, 272658, 1, 6, 6, true) --Electrified Scales    
+
 --Adderis and Aspix
 GridStatusRaidDebuff:BossNameId(zoneid, 10, "Adderis and Aspix")
 GridStatusRaidDebuff:DebuffId(zoneid, 263371, 11, 6, 6, true)  --Conduction 
