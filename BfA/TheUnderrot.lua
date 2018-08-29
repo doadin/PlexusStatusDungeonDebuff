ce79c236c7d3d838ecc4632641ebf0c4c5c0731d
Unknown <tbkizle@gmail.com>
2018-08-29 15:02:14 -0400

    Update The underrot

--- BfA/TheUnderrot.lua
+++ BfA/TheUnderrot.lua
@@ -1,16 +1,45 @@
 --local zone = "The Underrot"
 local zoneid = 1041
 
+GridStatusRaidDebuff:DebuffId(zoneid, 272457, 1, 6, 6, true) --Shockwave
+GridStatusRaidDebuff:DebuffId(zoneid, 260312, 1, 6, 6, true) --Charge
+GridStatusRaidDebuff:DebuffId(zoneid, 272180, 1, 6, 6, true) --Death Bolt
+GridStatusRaidDebuff:DebuffId(zoneid, 259732, 1, 6, 6, true) --Festering Harvest
+GridStatusRaidDebuff:DebuffId(zoneid, 272469, 1, 6, 6, true) --Abyssal Slam
+GridStatusRaidDebuff:DebuffId(zoneid, 266265, 1, 6, 6, true) --Wicked Assault
+GridStatusRaidDebuff:DebuffId(zoneid, 187647, 1, 6, 6, true) --Bloodletting Pounce
+GridStatusRaidDebuff:DebuffId(zoneid, 272609, 1, 6, 6, true) --Maddening Gaze
+GridStatusRaidDebuff:DebuffId(zoneid, 120694, 1, 6, 6, true) --Dire Beast
+GridStatusRaidDebuff:DebuffId(zoneid, 260793, 1, 6, 6, true) --Indigestion
+GridStatusRaidDebuff:DebuffId(zoneid, 266201, 1, 6, 6, true) --Bone Shield
+GridStatusRaidDebuff:DebuffId(zoneid, 261498, 1, 6, 6, true) --Creeping Rot
+GridStatusRaidDebuff:DebuffId(zoneid, 265377, 1, 6, 6, true) --Hooked Snare
+GridStatusRaidDebuff:DebuffId(zoneid, 266107, 1, 6, 6, true) --Thirst For Blood
+GridStatusRaidDebuff:DebuffId(zoneid, 265084, 1, 6, 6, true) --Blood Bolt
+GridStatusRaidDebuff:DebuffId(zoneid, 265376, 1, 6, 6, true) --Barbed Spear
+GridStatusRaidDebuff:DebuffId(zoneid, 265540, 1, 6, 6, true) --Rotten Bile
+GridStatusRaidDebuff:DebuffId(zoneid, 265568, 1, 6, 6, true) --Dark Omen
+GridStatusRaidDebuff:DebuffId(zoneid, 278789, 1, 6, 6, true) --Wave of Decay
+GridStatusRaidDebuff:DebuffId(zoneid, 265468, 1, 6, 6, true) --Withering Curse
+GridStatusRaidDebuff:DebuffId(zoneid, 266106, 1, 6, 6, true) --Sonic Screech
+GridStatusRaidDebuff:DebuffId(zoneid, 265511, 1, 6, 6, true) --Spirit Drain
+GridStatusRaidDebuff:DebuffId(zoneid, 265016, 1, 6, 6, true) --Blood Harvest
+GridStatusRaidDebuff:DebuffId(zoneid, 265533, 1, 6, 6, true) --Blood Maw
+GridStatusRaidDebuff:DebuffId(zoneid, 269843, 1, 6, 6, true) --Vile Expulsion
+GridStatusRaidDebuff:DebuffId(zoneid, 278961, 1, 6, 6, true) --Decaying Mind
+GridStatusRaidDebuff:DebuffId(zoneid, 265487, 1, 6, 6, true) --Shadow Bolt Volley
+GridStatusRaidDebuff:DebuffId(zoneid, 265019, 1, 6, 6, true) --Savage Cleave
+
 --Elder Leaxa
 GridStatusRaidDebuff:BossNameId(zoneid, 10, "Saboteur Kip'tilak")
-GridStatusRaidDebuff:DebuffId(zoneid, 260685, 11, 6, 6, true) --Taint of G'huun 
+GridStatusRaidDebuff:DebuffId(zoneid, 260685, 11, 6, 6, true) --Taint of G'huun
 --Cragmaw the Infested
 GridStatusRaidDebuff:BossNameId(zoneid, 20, "Saboteur Kip'tilak")
-GridStatusRaidDebuff:DebuffId(zoneid, 260333, 21, 6, 6, true) --Tantrum 
+GridStatusRaidDebuff:DebuffId(zoneid, 260333, 21, 6, 6, true) --Tantrum
 --Sporecaller Zancha
 GridStatusRaidDebuff:BossNameId(zoneid, 30, "Saboteur Kip'tilak")
-GridStatusRaidDebuff:DebuffId(zoneid, 259714, 31, 6, 6, true) --Decaying Spores 
-GridStatusRaidDebuff:DebuffId(zoneid, 259718, 32, 6, 6, true) --Upheaval 
+GridStatusRaidDebuff:DebuffId(zoneid, 259714, 31, 6, 6, true) --Decaying Spores
+GridStatusRaidDebuff:DebuffId(zoneid, 259718, 32, 6, 6, true) --Upheaval
 --Unbound Abomination
 GridStatusRaidDebuff:BossNameId(zoneid, 40, "Saboteur Kip'tilak")
 GridStatusRaidDebuff:DebuffId(zoneid, 269301, 41, 6, 6, true) --Putrid Blood
