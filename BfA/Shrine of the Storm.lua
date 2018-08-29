813f7543f30b2d28728b050100364c3fd04ac6f1
Unknown <tbkizle@gmail.com>
2018-08-29 12:32:38 -0400

    Update Shrine of the Storm

--- BfA/Shrine of the Storm.lua
+++ BfA/Shrine of the Storm.lua
@@ -1,25 +1,55 @@
 --local zone = "Shrine of the Storm"
 local zoneid = 1039
 
+GridStatusRaidDebuff:DebuffId(zoneid, 268317, 1, 6, 6, true) --Rip Mind
+GridStatusRaidDebuff:DebuffId(zoneid, 268322, 1, 6, 6, true) --Touch of the Drowned
+GridStatusRaidDebuff:DebuffId(zoneid, 268233, 1, 6, 6, true) --Electrifying Shock
+GridStatusRaidDebuff:DebuffId(zoneid, 268273, 1, 6, 6, true) --Deep Smash
+GridStatusRaidDebuff:DebuffId(zoneid, 267969, 1, 6, 6, true) --Water Blast
+GridStatusRaidDebuff:DebuffId(zoneid, 267973, 1, 6, 6, true) --Wash Away
+GridStatusRaidDebuff:DebuffId(zoneid, 265001, 1, 6, 6, true) --Sea Blast
+GridStatusRaidDebuff:DebuffId(zoneid, 274703, 1, 6, 6, true) --Void Bolt
+GridStatusRaidDebuff:DebuffId(zoneid, 224729, 1, 6, 6, true) --Bursting Shot
+GridStatusRaidDebuff:DebuffId(zoneid, 276286, 1, 6, 6, true) --Slicing Hurricane
+GridStatusRaidDebuff:DebuffId(zoneid, 269104, 1, 6, 6, true) --Explosive Void
+GridStatusRaidDebuff:DebuffId(zoneid, 274438, 1, 6, 6, true) --Tempest
+GridStatusRaidDebuff:DebuffId(zoneid, 277373, 1, 6, 6, true) --Massive Glaive
+GridStatusRaidDebuff:DebuffId(zoneid, 268028, 1, 6, 6, true) --Crushing Waters
+GridStatusRaidDebuff:DebuffId(zoneid, 268391, 1, 6, 6, true) --Mental Assault
+GridStatusRaidDebuff:DebuffId(zoneid, 268309, 1, 6, 6, true) --Unending Darkness
+GridStatusRaidDebuff:DebuffId(zoneid, 268280, 1, 6, 6, true) --Tidal Pod
+GridStatusRaidDebuff:DebuffId(zoneid, 267956, 1, 6, 6, true) --Zap
+GridStatusRaidDebuff:DebuffId(zoneid, 268315, 1, 6, 6, true) --Lash
+GridStatusRaidDebuff:DebuffId(zoneid, 274720, 1, 6, 6, true) --Abyssal Strike
+GridStatusRaidDebuff:DebuffId(zoneid, 269419, 1, 6, 6, true) --Yawning Gate
+GridStatusRaidDebuff:DebuffId(zoneid, 268348, 1, 6, 6, true) --Colossal Slam
+GridStatusRaidDebuff:DebuffId(zoneid, 268214, 1, 6, 6, true) --Carve Flesh
+GridStatusRaidDebuff:DebuffId(zoneid, 268059, 1, 6, 6, true) --Anchor of Binding
+GridStatusRaidDebuff:DebuffId(zoneid, 268027, 1, 6, 6, true) --Rising Tides
+GridStatusRaidDebuff:DebuffId(zoneid, 274633, 1, 6, 6, true) --Sundering Blow
+GridStatusRaidDebuff:DebuffId(zoneid, 267818, 1, 6, 6, true) --Slicing Blast
+GridStatusRaidDebuff:DebuffId(zoneid, 274792, 1, 6, 6, true) --Bite
+
+
 --Aqu'sirr
 GridStatusRaidDebuff:BossNameId(zoneid, 10, "Aqu'sirr")
-GridStatusRaidDebuff:DebuffId(zoneid, 264560, 11, 6, 6, true) --Choking Brine 
-GridStatusRaidDebuff:DebuffId(zoneid, 264155, 12, 6, 6, true) --Surging Rush 
-GridStatusRaidDebuff:DebuffId(zoneid, 264166, 13, 6, 6, true) --Undertow 
+GridStatusRaidDebuff:DebuffId(zoneid, 264560, 11, 6, 6, true) --Choking Brine
+GridStatusRaidDebuff:DebuffId(zoneid, 264155, 12, 6, 6, true) --Surging Rush
+GridStatusRaidDebuff:DebuffId(zoneid, 264166, 13, 6, 6, true) --Undertow
 --Tidesage Council
 GridStatusRaidDebuff:BossNameId(zoneid, 20, "Tidesage Council")
-GridStatusRaidDebuff:DebuffId(zoneid, 267905, 21, 6, 6, true) --Reinforcing Ward 
-GridStatusRaidDebuff:DebuffId(zoneid, 267891, 22, 6, 6, true) --Swiftness Ward 
-GridStatusRaidDebuff:DebuffId(zoneid, 267899, 23, 6, 6, true) --Hindering Cleave 
-GridStatusRaidDebuff:DebuffId(zoneid, 267830, 24, 6, 6, true) --Blessing of the Tempest 
-GridStatusRaidDebuff:DebuffId(zoneid, 267841, 25, 6, 6, true) --Blowback 
+GridStatusRaidDebuff:DebuffId(zoneid, 267905, 21, 6, 6, true) --Reinforcing Ward
+GridStatusRaidDebuff:DebuffId(zoneid, 267891, 22, 6, 6, true) --Swiftness Ward
+GridStatusRaidDebuff:DebuffId(zoneid, 267899, 23, 6, 6, true) --Hindering Cleave
+GridStatusRaidDebuff:DebuffId(zoneid, 267830, 24, 6, 6, true) --Blessing of the Tempest
+GridStatusRaidDebuff:DebuffId(zoneid, 267841, 25, 6, 6, true) --Blowback
 --Lord Stormsong
 GridStatusRaidDebuff:BossNameId(zoneid, 30, "Lord Stormsong")
-GridStatusRaidDebuff:DebuffId(zoneid, 269097, 31, 6, 6, true) --Waken the Void 
-GridStatusRaidDebuff:DebuffId(zoneid, 269131, 32, 6, 6, true) --Ancient mindbender 
-GridStatusRaidDebuff:DebuffId(zoneid, 268896, 33, 6, 6, true) --Mind Rend 
+GridStatusRaidDebuff:DebuffId(zoneid, 269097, 31, 6, 6, true) --Waken the Void
+GridStatusRaidDebuff:DebuffId(zoneid, 269131, 32, 6, 6, true) --Ancient mindbender
+GridStatusRaidDebuff:DebuffId(zoneid, 268896, 33, 6, 6, true) --Mind Rend
 --Vol'zith the Whisperer
 GridStatusRaidDebuff:BossNameId(zoneid, 40, "Vol'zith the Whisperer")
-GridStatusRaidDebuff:DebuffId(zoneid, 267548, 41, 6, 6, true) --Leviathan's Wake 
-GridStatusRaidDebuff:DebuffId(zoneid, 267360, 42, 6, 6, true) --Grasp of the Sunken City 
-GridStatusRaidDebuff:DebuffId(zoneid, 267034, 43, 6, 6, true) --Whispers of Power 
+GridStatusRaidDebuff:DebuffId(zoneid, 267548, 41, 6, 6, true) --Leviathan's Wake
+GridStatusRaidDebuff:DebuffId(zoneid, 267360, 42, 6, 6, true) --Grasp of the Sunken City
+GridStatusRaidDebuff:DebuffId(zoneid, 267034, 43, 6, 6, true) --Whispers of Power
