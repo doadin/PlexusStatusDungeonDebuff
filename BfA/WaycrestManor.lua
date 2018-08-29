c7a7e4c813022d5a515c1a79b1a2bf73f9266e21
Unknown <tbkizle@gmail.com>
2018-08-29 16:07:03 -0400

    Update Waycrest Manor

--- BfA/WaycrestManor.lua
+++ BfA/WaycrestManor.lua
@@ -1,26 +1,74 @@
 --local zone = "Waycrest Manor"
 local zoneid = 1015
 
+GridStatusRaidDebuff:DebuffId(zoneid, 264105, 1, 6, 6, true)  --Runic Mark
+GridStatusRaidDebuff:DebuffId(zoneid, 271174, 1, 6, 6, true)  --Retch
+GridStatusRaidDebuff:DebuffId(zoneid, 264040, 1, 6, 6, true)  --Uprooted Thorns
+GridStatusRaidDebuff:DebuffId(zoneid, 260900, 1, 6, 6, true)  --Soul Manipulation
+GridStatusRaidDebuff:DebuffId(zoneid, 264378, 1, 6, 6, true)  --Fragment Soul
+GridStatusRaidDebuff:DebuffId(zoneid, 271178, 1, 6, 6, true)  --Ravaging Leap
+GridStatusRaidDebuff:DebuffId(zoneid, 260569, 1, 6, 6, true)  --Wildfire
+GridStatusRaidDebuff:DebuffId(zoneid, 264153, 1, 6, 6, true)  --Spit
+GridStatusRaidDebuff:DebuffId(zoneid, 265391, 1, 6, 6, true)  --Meaty Rampage
+GridStatusRaidDebuff:DebuffId(zoneid, 264531, 1, 6, 6, true)  --Shrapnel Trap
+GridStatusRaidDebuff:DebuffId(zoneid, 265371, 1, 6, 6, true)  --Captain's Strike
+GridStatusRaidDebuff:DebuffId(zoneid, 267824, 1, 6, 6, true)  --Scar Soul
+GridStatusRaidDebuff:DebuffId(zoneid, 265337, 1, 6, 6, true)  --Snout Smack
+GridStatusRaidDebuff:DebuffId(zoneid, 200010, 1, 6, 6, true)  --Power Leech
+GridStatusRaidDebuff:DebuffId(zoneid, 266036, 1, 6, 6, true)  --Drain Essence
+GridStatusRaidDebuff:DebuffId(zoneid, 265410, 1, 6, 6, true)  --Punch
+GridStatusRaidDebuff:DebuffId(zoneid, 263943, 1, 6, 6, true)  --Etch
+GridStatusRaidDebuff:DebuffId(zoneid, 268271, 1, 6, 6, true)  --Wracking Chord
+GridStatusRaidDebuff:DebuffId(zoneid, 261438, 1, 6, 6, true)  --Wasting Strike
+GridStatusRaidDebuff:DebuffId(zoneid, 264111, 1, 6, 6, true)  --Darkened Blade
+GridStatusRaidDebuff:DebuffId(zoneid, 265757, 1, 6, 6, true)  --Splinter Spike
+GridStatusRaidDebuff:DebuffId(zoneid, 265881, 1, 6, 6, true)  --Decaying Touch
+GridStatusRaidDebuff:DebuffId(zoneid, 264556, 1, 6, 6, true)  --Tearing Strike
+GridStatusRaidDebuff:DebuffId(zoneid, 264150, 1, 6, 6, true)  --Shatter
+GridStatusRaidDebuff:DebuffId(zoneid, 264510, 1, 6, 6, true)  --Shoot
+GridStatusRaidDebuff:DebuffId(zoneid, 264456, 1, 6, 6, true)  --Tracking Explosive
+GridStatusRaidDebuff:DebuffId(zoneid, 260701, 1, 6, 6, true)  --Bramble Bolt
+GridStatusRaidDebuff:DebuffId(zoneid, 265882, 1, 6, 6, true)  --Lingering Dread
+GridStatusRaidDebuff:DebuffId(zoneid, 265642, 1, 6, 6, true)  --Diseased Crunch
+GridStatusRaidDebuff:DebuffId(zoneid, 260699, 1, 6, 6, true)  --Soul Bolt
+GridStatusRaidDebuff:DebuffId(zoneid, 264520, 1, 6, 6, true)  --Severing Serpent
+GridStatusRaidDebuff:DebuffId(zoneid, 260700, 1, 6, 6, true)  --Ruinous Bolt
+GridStatusRaidDebuff:DebuffId(zoneid, 265407, 1, 6, 6, true)  --Dinner Bell
+GridStatusRaidDebuff:DebuffId(zoneid, 260703, 1, 6, 6, true)  --Unstable Runic Mark
+GridStatusRaidDebuff:DebuffId(zoneid, 266035, 1, 6, 6, true)  --Bone Splinter
+GridStatusRaidDebuff:DebuffId(zoneid, 263959, 1, 6, 6, true)  --Soul Volley
+GridStatusRaidDebuff:DebuffId(zoneid, 265352, 1, 6, 6, true)  --Toad Blight
+GridStatusRaidDebuff:DebuffId(zoneid, 265880, 1, 6, 6, true)  --Dread Mark
+GridStatusRaidDebuff:DebuffId(zoneid, 265372, 1, 6, 6, true)  --Shadow Cleave
+GridStatusRaidDebuff:DebuffId(zoneid, 264390, 1, 6, 6, true)  --Spellbind
+GridStatusRaidDebuff:DebuffId(zoneid, 278849, 1, 6, 6, true)  --Uproot
+GridStatusRaidDebuff:DebuffId(zoneid, 265876, 1, 6, 6, true)  --Ruinous Volley
+GridStatusRaidDebuff:DebuffId(zoneid, 264655, 1, 6, 6, true)  --Withering Glob
+GridStatusRaidDebuff:DebuffId(zoneid, 265761, 1, 6, 6, true)  --Thorned Barrage
+GridStatusRaidDebuff:DebuffId(zoneid, 264140, 1, 6, 6, true)  --Thorned Claw
+GridStatusRaidDebuff:DebuffId(zoneid, 263905, 1, 6, 6, true)  --Marking Cleave
+GridStatusRaidDebuff:DebuffId(zoneid, 264050, 1, 6, 6, true)  --Infected Thorn
+
 --Heartsbane Triad
 GridStatusRaidDebuff:BossNameId(zoneid, 10, "Heartsbane Triad")
-GridStatusRaidDebuff:DebuffId(zoneid, 260741, 11, 6, 6, true)  --Jagged Nettles 
-GridStatusRaidDebuff:DebuffId(zoneid, 260805, 12, 6, 6, true)  --Focusing Iris 
+GridStatusRaidDebuff:DebuffId(zoneid, 260741, 11, 6, 6, true)  --Jagged Nettles
+GridStatusRaidDebuff:DebuffId(zoneid, 260805, 12, 6, 6, true)  --Focusing Iris
 --Soulbound Goliath
 GridStatusRaidDebuff:BossNameId(zoneid, 20, "Soulbound Goliath")
-GridStatusRaidDebuff:DebuffId(zoneid, 260512, 21, 6, 6, true)  --Soul Harvest 
-GridStatusRaidDebuff:DebuffId(zoneid, 260551, 22, 6, 6, true)  --Soul Thorns 
-GridStatusRaidDebuff:DebuffId(zoneid, 260508, 23, 6, 6, true)  --Crush 
-GridStatusRaidDebuff:DebuffId(zoneid, 260547, 24, 6, 6, true)  --Burning Brush 
+GridStatusRaidDebuff:DebuffId(zoneid, 260512, 21, 6, 6, true)  --Soul Harvest
+GridStatusRaidDebuff:DebuffId(zoneid, 260551, 22, 6, 6, true)  --Soul Thorns
+GridStatusRaidDebuff:DebuffId(zoneid, 260508, 23, 6, 6, true)  --Crush
+GridStatusRaidDebuff:DebuffId(zoneid, 260547, 24, 6, 6, true)  --Burning Brush
 --Raal the Glutonous
 GridStatusRaidDebuff:BossNameId(zoneid, 30, "Raal the Glutonous")
-GridStatusRaidDebuff:DebuffId(zoneid, 264923, 31, 6, 6, true)  --Tenderize 
-GridStatusRaidDebuff:DebuffId(zoneid, 264694, 32, 6, 6, true)  --Rotten Expulsion 
+GridStatusRaidDebuff:DebuffId(zoneid, 264923, 31, 6, 6, true)  --Tenderize
+GridStatusRaidDebuff:DebuffId(zoneid, 264694, 32, 6, 6, true)  --Rotten Expulsion
 --Lord and Lady Waycrest
 GridStatusRaidDebuff:BossNameId(zoneid, 40, "Lord and Lady Waycrest")
-GridStatusRaidDebuff:DebuffId(zoneid, 261439, 41, 6, 6, true)  --Virulent Pathogen 
-GridStatusRaidDebuff:DebuffId(zoneid, 268306, 42, 6, 6, true)  --Discordant Cadenza 
+GridStatusRaidDebuff:DebuffId(zoneid, 261439, 41, 6, 6, true)  --Virulent Pathogen
+GridStatusRaidDebuff:DebuffId(zoneid, 268306, 42, 6, 6, true)  --Discordant Cadenza
 --Gorak Tul
 GridStatusRaidDebuff:BossNameId(zoneid, 50, "Gorak Tul")
-GridStatusRaidDebuff:DebuffId(zoneid, 266225, 51, 6, 6, true)  --Darkened Lightning 
-GridStatusRaidDebuff:DebuffId(zoneid, 266198, 52, 6, 6, true)  --Alchemical Fire 
-GridStatusRaidDebuff:DebuffId(zoneid, 268202, 53, 6, 6, true)  --Death Lens 
+GridStatusRaidDebuff:DebuffId(zoneid, 266225, 51, 6, 6, true)  --Darkened Lightning
+GridStatusRaidDebuff:DebuffId(zoneid, 266198, 52, 6, 6, true)  --Alchemical Fire
+GridStatusRaidDebuff:DebuffId(zoneid, 268202, 53, 6, 6, true)  --Death Lens
