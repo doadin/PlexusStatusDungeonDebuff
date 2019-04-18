00a1dc664a5f1a3f9c8b2afdf1fa751277b9ef5f
Unknown <tbkizle@gmail.com>
2019-04-17 21:43:22 -0400

    Add Support for The Nexus

---
+++ Wrath/TheNexus.lua
@@ -0,0 +1,31 @@
+--local zone = "The Nexus"
+local zoneid = 435
+
+-- zoneid, debuffID, order, icon_priority, color_priority, timer, stackable, color, default_disable, noicon
+--true, true is for stackable
+
+GridStatusRaidDebuff:DebuffId(zoneid, 60067, 1, 6, 6) --Charge
+GridStatusRaidDebuff:DebuffId(zoneid, 47731, 1, 6, 6) --Critter
+GridStatusRaidDebuff:DebuffId(zoneid, 57091, 1, 6, 6) --Crystalfire Breath
+GridStatusRaidDebuff:DebuffId(zoneid, 56775, 1, 6, 6) --Frostbolt
+GridStatusRaidDebuff:DebuffId(zoneid, 13323, 1, 6, 6) --Polymorph
+GridStatusRaidDebuff:DebuffId(zoneid, 48053, 1, 6, 6) --Ensnare
+GridStatusRaidDebuff:DebuffId(zoneid, 56776, 1, 6, 6) --Arcane Shock
+GridStatusRaidDebuff:DebuffId(zoneid, 56777, 1, 6, 6) --Silence
+GridStatusRaidDebuff:DebuffId(zoneid, 29882, 1, 6, 6) --Loose Mana
+GridStatusRaidDebuff:DebuffId(zoneid, 47700, 1, 6, 6) --Crystal Freeze
+GridStatusRaidDebuff:DebuffId(zoneid, 47736, 1, 6, 6) --Time Stop
+GridStatusRaidDebuff:DebuffId(zoneid, 55041, 1, 6, 6) --Freezing Trap Effect
+GridStatusRaidDebuff:DebuffId(zoneid, 56933, 1, 6, 6) --Incendiary Shot
+GridStatusRaidDebuff:DebuffId(zoneid, 48095, 1, 6, 6) --Intense Cold
+GridStatusRaidDebuff:DebuffId(zoneid, 57050, 1, 6, 6) --Crystal Chains
+GridStatusRaidDebuff:DebuffId(zoneid, 29544, 1, 6, 6) --Frightening Shout
+GridStatusRaidDebuff:DebuffId(zoneid, 50132, 1, 6, 6) --Draw Magic
+GridStatusRaidDebuff:DebuffId(zoneid, 30849, 1, 6, 6) --Spell Lock
+GridStatusRaidDebuff:DebuffId(zoneid, 56860, 1, 6, 6) --Magic Burn
+GridStatusRaidDebuff:DebuffId(zoneid, 48179, 1, 6, 6) --Crystallize
+GridStatusRaidDebuff:DebuffId(zoneid, 57063, 1, 6, 6) --Arcane Attraction
+GridStatusRaidDebuff:DebuffId(zoneid, 30633, 1, 6, 6) --Thunderclap
+GridStatusRaidDebuff:DebuffId(zoneid, 47779, 1, 6, 6) --Arcane Torrent
+GridStatusRaidDebuff:DebuffId(zoneid, 38384, 1, 6, 6) --Cone of Cold
+GridStatusRaidDebuff:DebuffId(zoneid, 61556, 1, 6, 6) --Tangle
