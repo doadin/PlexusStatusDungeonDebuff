971568f318a6721430ca283e03bdcbacc640046b
Unknown <tbkizle@gmail.com>
2019-04-17 22:38:11 -0400

    Add Support for Halls of Stone

---
+++ Wrath/HallsofStone.lua
@@ -0,0 +1,14 @@
+--local zone = "Halls of Stone
+local zoneid = 140
+
+-- zoneid, debuffID, order, icon_priority, color_priority, timer, stackable, color, default_disable, noicon
+--true, true is for stackable
+
+GridStatusRaidDebuff:DebuffId(zoneid, 46202, 1, 6, 6) --Pierce Armor
+GridStatusRaidDebuff:DebuffId(zoneid, 50895, 1, 6, 6) --Lightning Tether
+GridStatusRaidDebuff:DebuffId(zoneid, 50761, 1, 6, 6) --Pillar of Woe
+GridStatusRaidDebuff:DebuffId(zoneid, 50834, 1, 6, 6) --Static Charge
+GridStatusRaidDebuff:DebuffId(zoneid, 50841, 1, 6, 6) --Lightning Ring
+GridStatusRaidDebuff:DebuffId(zoneid, 51491, 1, 6, 6) --Unrelenting Strike
+GridStatusRaidDebuff:DebuffId(zoneid, 51496, 1, 6, 6) --Chiseling Ray
+GridStatusRaidDebuff:DebuffId(zoneid, 50760, 1, 6, 6) --Shock of Sorrow
