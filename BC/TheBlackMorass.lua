8a6bc155616f7b54ab1b9abdf8aa6d435c06490f
Unknown <tbkizle@gmail.com>
2019-04-17 21:58:24 -0400

    Add Support for The Black Morass

---
+++ BC/TheBlackMorass.lua
@@ -0,0 +1,28 @@
+--local zone = "The Black Morass"
+local zoneid = 273
+
+-- zoneid, debuffID, order, icon_priority, color_priority, timer, stackable, color, default_disable, noicon
+--true, true is for stackable
+
+
+GridStatusRaidDebuff:DebuffId(zoneid, 102066, 1, 6, 6) --Corruption"] = 32197,
+GridStatusRaidDebuff:DebuffId(zoneid, 102066, 1, 6, 6) --Time Lapse"] = 31467,
+GridStatusRaidDebuff:DebuffId(zoneid, 102066, 1, 6, 6) --Deadly Poison"] = 38520,
+GridStatusRaidDebuff:DebuffId(zoneid, 102066, 1, 6, 6) --Frostbolt"] = 38534,
+GridStatusRaidDebuff:DebuffId(zoneid, 102066, 1, 6, 6) --Polymorph"] = 13323,
+GridStatusRaidDebuff:DebuffId(zoneid, 102066, 1, 6, 6) --Pyroblast"] = 38535,
+GridStatusRaidDebuff:DebuffId(zoneid, 102066, 1, 6, 6) --Rupture"] = 15583,
+GridStatusRaidDebuff:DebuffId(zoneid, 102066, 1, 6, 6) --Mortal Wound"] = 31464,
+GridStatusRaidDebuff:DebuffId(zoneid, 102066, 1, 6, 6) --Fear"] = 12542,
+GridStatusRaidDebuff:DebuffId(zoneid, 102066, 1, 6, 6) --Time Stop"] = 31422,
+GridStatusRaidDebuff:DebuffId(zoneid, 102066, 1, 6, 6) --Hamstring"] = 9080,
+GridStatusRaidDebuff:DebuffId(zoneid, 102066, 1, 6, 6) --Ebon Poison"] = 34366,
+GridStatusRaidDebuff:DebuffId(zoneid, 102066, 1, 6, 6) --Crippling Poison"] = 30981,
+GridStatusRaidDebuff:DebuffId(zoneid, 102066, 1, 6, 6) --Mortal Strike"] = 35054,
+GridStatusRaidDebuff:DebuffId(zoneid, 102066, 1, 6, 6) --Curse of Vulnerability"] = 36276,
+GridStatusRaidDebuff:DebuffId(zoneid, 102066, 1, 6, 6) --Knockdown"] = 11428,
+GridStatusRaidDebuff:DebuffId(zoneid, 102066, 1, 6, 6) --Blast Wave"] = 38536,
+GridStatusRaidDebuff:DebuffId(zoneid, 102066, 1, 6, 6) --Frost Nova"] = 15531,
+GridStatusRaidDebuff:DebuffId(zoneid, 102066, 1, 6, 6) --Sunder Armor"] = 16145,
+GridStatusRaidDebuff:DebuffId(zoneid, 102066, 1, 6, 6) --Sand Breath"] = 39049,
+GridStatusRaidDebuff:DebuffId(zoneid, 102066, 1, 6, 6) --Kidney Shot"] = 30832,
