feb6376d13d086b863300f028eb3d0f38295d4ac
Unknown <tbkizle@gmail.com>
2019-04-17 21:31:28 -0400

    Update Scarlet Monastery

--- MoP/ScarletMonastery.lua
+++ MoP/ScarletMonastery.lua
@@ -4,6 +4,9 @@ local zoneid = 435
 -- zoneid, debuffID, order, icon_priority, color_priority, timer, stackable, color, default_disable, noicon
 --true, true is for stackable
 
+GridStatusRaidDebuff:DebuffId(zoneid, 42380, 1, 6, 6) --Conflagration
+GridStatusRaidDebuff:DebuffId(zoneid, 42514, 1, 6, 6) --Squash Soul
+
 --Thalnos
 GridStatusRaidDebuff:BossNameId(zoneid, 10, "Thalnos")
 GridStatusRaidDebuff:DebuffId(zoneid, 115144, 11, 6, 6) 
