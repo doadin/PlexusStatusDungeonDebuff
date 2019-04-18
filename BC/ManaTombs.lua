00d43f0e21345b083f2e5869cab648af39d07542
Unknown <tbkizle@gmail.com>
2019-04-17 22:13:17 -0400

    Add Support for Mana-Tombs

---
+++ BC/ManaTombs.lua
@@ -0,0 +1,24 @@
+--local zone = "Mana-Tombs"
+local zoneid = 272
+
+-- zoneid, debuffID, order, icon_priority, color_priority, timer, stackable, color, default_disable, noicon
+--true, true is for stackable
+
+GridStatusRaidDebuff:DebuffId(zoneid, 25602, 1, 6, 6) --Faerie Fire
+GridStatusRaidDebuff:DebuffId(zoneid, 34942, 1, 6, 6) --Shadow Word: Pain
+GridStatusRaidDebuff:DebuffId(zoneid, 33865, 1, 6, 6) --Singe
+GridStatusRaidDebuff:DebuffId(zoneid, 37668, 1, 6, 6) --Immolate
+GridStatusRaidDebuff:DebuffId(zoneid, 13323, 1, 6, 6) --Polymorph
+GridStatusRaidDebuff:DebuffId(zoneid, 34925, 1, 6, 6) --Curse of Impotence
+GridStatusRaidDebuff:DebuffId(zoneid, 33919, 1, 6, 6) --Earthquake
+GridStatusRaidDebuff:DebuffId(zoneid, 25603, 1, 6, 6) --Slow
+GridStatusRaidDebuff:DebuffId(zoneid, 34940, 1, 6, 6) --Gouge
+GridStatusRaidDebuff:DebuffId(zoneid, 32315, 1, 6, 6) --Soul Strike
+GridStatusRaidDebuff:DebuffId(zoneid, 32361, 1, 6, 6) --Crystal Prison
+GridStatusRaidDebuff:DebuffId(zoneid, 34922, 1, 6, 6) --Shadows Embrace
+GridStatusRaidDebuff:DebuffId(zoneid, 38065, 1, 6, 6) --Death Coil
+GridStatusRaidDebuff:DebuffId(zoneid, 38064, 1, 6, 6) --Blast Wave
+GridStatusRaidDebuff:DebuffId(zoneid, 32365, 1, 6, 6) --Frost Nova
+GridStatusRaidDebuff:DebuffId(zoneid, 39332, 1, 6, 6) --Phantom Strike
+GridStatusRaidDebuff:DebuffId(zoneid, 32364, 1, 6, 6) --Frostbolt
+GridStatusRaidDebuff:DebuffId(zoneid, 22911, 1, 6, 6) --Charge
