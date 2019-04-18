738540feb8beb083869ccaa2a001e39ef711a0cd
Unknown <tbkizle@gmail.com>
2019-04-17 22:19:50 -0400

    Add Support for Utgarde Pinnacle

---
+++ Wrath/UtgardePinnacle.lua
@@ -0,0 +1,33 @@
+--local zone = "Utgarde Pinnacle"
+local zoneid = 136
+
+-- zoneid, debuffID, order, icon_priority, color_priority, timer, stackable, color, default_disable, noicon
+--true, true is for stackable
+
+GridStatusRaidDebuff:DebuffId(zoneid, 49089, 1, 6, 6) --Shrink
+GridStatusRaidDebuff:DebuffId(zoneid, 59239, 1, 6, 6) --Rend
+GridStatusRaidDebuff:DebuffId(zoneid, 59267, 1, 6, 6) --Withering Roar
+GridStatusRaidDebuff:DebuffId(zoneid, 59330, 1, 6, 6) --Crush
+GridStatusRaidDebuff:DebuffId(zoneid, 48291, 1, 6, 6) --Fetid Rot
+GridStatusRaidDebuff:DebuffId(zoneid, 59263, 1, 6, 6) --Grievous Wound
+GridStatusRaidDebuff:DebuffId(zoneid, 48278, 1, 6, 6) --Paralyze
+GridStatusRaidDebuff:DebuffId(zoneid, 59265, 1, 6, 6) --Mortal Wound
+GridStatusRaidDebuff:DebuffId(zoneid, 59271, 1, 6, 6) --Poison Breath
+GridStatusRaidDebuff:DebuffId(zoneid, 56785, 1, 6, 6) --Volatile Infection
+GridStatusRaidDebuff:DebuffId(zoneid, 59268, 1, 6, 6) --Impale
+GridStatusRaidDebuff:DebuffId(zoneid, 48132, 1, 6, 6) --Acid Spit
+GridStatusRaidDebuff:DebuffId(zoneid, 48131, 1, 6, 6) --Stomp
+GridStatusRaidDebuff:DebuffId(zoneid, 48639, 1, 6, 6) --Hamstring
+GridStatusRaidDebuff:DebuffId(zoneid, 48871, 1, 6, 6) --Aimed Shot
+GridStatusRaidDebuff:DebuffId(zoneid, 49092, 1, 6, 6) --Net
+GridStatusRaidDebuff:DebuffId(zoneid, 48144, 1, 6, 6) --Terrifying Roar
+GridStatusRaidDebuff:DebuffId(zoneid, 48277, 1, 6, 6) --Ritual Strike
+GridStatusRaidDebuff:DebuffId(zoneid, 59408, 1, 6, 6) --Shadows in the Dark
+GridStatusRaidDebuff:DebuffId(zoneid, 51750, 1, 6, 6) --Screams of the Dead
+GridStatusRaidDebuff:DebuffId(zoneid, 59264, 1, 6, 6) --Gore
+GridStatusRaidDebuff:DebuffId(zoneid, 59079, 1, 6, 6) --Grave Strike
+GridStatusRaidDebuff:DebuffId(zoneid, 59237, 1, 6, 6) --Beast's Mark
+GridStatusRaidDebuff:DebuffId(zoneid, 59334, 1, 6, 6) --Poisoned Spear
+GridStatusRaidDebuff:DebuffId(zoneid, 49106, 1, 6, 6) --Terrify
+GridStatusRaidDebuff:DebuffId(zoneid, 61549, 1, 6, 6) --Shred
+GridStatusRaidDebuff:DebuffId(zoneid, 59304, 1, 6, 6) --Spirit Strike
