36e22a6548382ad6b8735d8369d52d43813a6d33
Unknown <tbkizle@gmail.com>
2019-04-17 21:51:51 -0400

    Update End Time

--- Cata/EndTime.lua
+++ Cata/EndTime.lua
@@ -1,8 +1,21 @@
---local zone = "Gate of the Setting Sun"
+--local zone = "End Time"
 local zoneid = 404
 
 -- zoneid, debuffID, order, icon_priority, color_priority, timer, stackable, color, default_disable, noicon
 --true, true is for stackable
+
+GridStatusRaidDebuff:DebuffId(zoneid, 102066, 1, 6, 6) --Flesh Rip
+GridStatusRaidDebuff:DebuffId(zoneid, 101984, 1, 6, 6) --Distortion Bomb
+GridStatusRaidDebuff:DebuffId(zoneid, 102414, 1, 6, 6) --Dark Moonlight
+GridStatusRaidDebuff:DebuffId(zoneid, 277373, 1, 6, 6) --Massive Glaive
+GridStatusRaidDebuff:DebuffId(zoneid, 101619, 1, 6, 6) --Magma
+GridStatusRaidDebuff:DebuffId(zoneid, 102057, 1, 6, 6) --Scorched
+GridStatusRaidDebuff:DebuffId(zoneid, 109952, 1, 6, 6) --Cadaver Toss
+GridStatusRaidDebuff:DebuffId(zoneid, 108589, 1, 6, 6) --Tail Sweep
+GridStatusRaidDebuff:DebuffId(zoneid, 101840, 1, 6, 6) --Molten Blast
+GridStatusRaidDebuff:DebuffId(zoneid, 102158, 1, 6, 6) --Sear Flesh
+GridStatusRaidDebuff:DebuffId(zoneid, 102132, 1, 6, 6) --Break Armor
+GridStatusRaidDebuff:DebuffId(zoneid, 102600, 1, 6, 6) --Temporal Vortex
 
 --Echo of Baine
 --Echo of Jaina
