2683795276e520dd839f115f39b11e22eb1f07f2
tbkizle <tbkizle@gmail.com>
2023-11-07 20:18:28 -0500

    [DF] Add Dawn of the Infinite

---
+++ DF/DawnoftheInfinite.lua
@@ -0,0 +1,142 @@
+-- local zone = "Dawn of the Infinite"
+local zoneid = 2198
+
+--M+
+_G.GridStatusRaidDebuff:DebuffId(zoneid, 240447, 1, 6, 6, true) --Quake
+_G.GridStatusRaidDebuff:DebuffId(zoneid, 240559, 1, 6, 6, true, true) --Grievous Wound
+_G.GridStatusRaidDebuff:DebuffId(zoneid, 209858, 1, 6, 6, true, true) --Necrotic Wound
+_G.GridStatusRaidDebuff:DebuffId(zoneid, 240443, 1, 6, 6, true, true) --Burst
+_G.GridStatusRaidDebuff:DebuffId(zoneid, 226512, 1, 6, 6, true) --Sanguine Ichor
+
+--Trash
+_G.GridStatusRaidDebuff:DebuffId(zoneid, 412044, 1, 6, 6, true, true) -- Temposlice
+_G.GridStatusRaidDebuff:DebuffId(zoneid, 411994, 1, 6, 6, true, true) -- Chronomelt
+_G.GridStatusRaidDebuff:DebuffId(zoneid, 415436, 1, 6, 6, true, true) -- Tainted Sands
+_G.GridStatusRaidDebuff:DebuffId(zoneid, 415554, 1, 6, 6, true, true) -- Chronoburst
+_G.GridStatusRaidDebuff:DebuffId(zoneid, 415437, 1, 6, 6, true, true) -- Enervate
+_G.GridStatusRaidDebuff:DebuffId(zoneid, 413547, 1, 6, 6, true, true) -- Bloom
+_G.GridStatusRaidDebuff:DebuffId(zoneid, 413529, 1, 6, 6, true, true) -- Untwist
+_G.GridStatusRaidDebuff:DebuffId(zoneid, 412810, 1, 6, 6, true, true) -- Blight Spew
+_G.GridStatusRaidDebuff:DebuffId(zoneid, 412285, 1, 6, 6, true, true) -- Stonebolt
+_G.GridStatusRaidDebuff:DebuffId(zoneid, 412505, 1, 6, 6, true, true) -- Rending Cleave 
+_G.GridStatusRaidDebuff:DebuffId(zoneid, 413606, 1, 6, 6, true, true) -- Corroding Volley
+_G.GridStatusRaidDebuff:DebuffId(zoneid, 414922, 1, 6, 6, true, true) -- Shrouding Sandstorm
+_G.GridStatusRaidDebuff:DebuffId(zoneid, 412922, 1, 6, 6, true, true) -- Binding Grasp
+_G.GridStatusRaidDebuff:DebuffId(zoneid, 412131, 1, 6, 6, true, true) -- Orb of Contemplation
+_G.GridStatusRaidDebuff:DebuffId(zoneid, 413027, 1, 6, 6, true, true) -- Titanic Bulwark
+_G.GridStatusRaidDebuff:DebuffId(zoneid, 413618, 1, 6, 6, true, true) -- Timeless Curse
+_G.GridStatusRaidDebuff:DebuffId(zoneid, 419351, 1, 6, 6, true, true) -- Bronze Exhalation
+_G.GridStatusRaidDebuff:DebuffId(zoneid, 418092, 1, 6, 6, true, true) -- Twisted Timeways
+_G.GridStatusRaidDebuff:DebuffId(zoneid, 418200, 1, 6, 6, true, true) -- Infinite Burn
+_G.GridStatusRaidDebuff:DebuffId(zoneid, 413427, 1, 6, 6, true, true) -- Time Beam
+_G.GridStatusRaidDebuff:DebuffId(zoneid, 419511, 1, 6, 6, true, true) -- Temporal Link
+_G.GridStatusRaidDebuff:DebuffId(zoneid, 419517, 1, 6, 6, true, true) -- Chronal Eruption
+_G.GridStatusRaidDebuff:DebuffId(zoneid, 407125, 1, 6, 6, true, true) -- Sundering Slam
+_G.GridStatusRaidDebuff:DebuffId(zoneid, 407651, 1, 6, 6, true, true) -- Sapper’s Perogative
+_G.GridStatusRaidDebuff:DebuffId(zoneid, 407715, 1, 6, 6, true, true) -- Kaboom!
+_G.GridStatusRaidDebuff:DebuffId(zoneid, 407906, 1, 6, 6, true, true) -- Earthquake
+_G.GridStatusRaidDebuff:DebuffId(zoneid, 407123, 1, 6, 6, true, true) -- Rain of Fire
+_G.GridStatusRaidDebuff:DebuffId(zoneid, 411700, 1, 6, 6, true, true) -- Slobbering Bite
+_G.GridStatusRaidDebuff:DebuffId(zoneid, 411644, 1, 6, 6, true, true) -- Soggy Bonk
+_G.GridStatusRaidDebuff:DebuffId(zoneid, 412262, 1, 6, 6, true, true) -- Staticky Punch
+
+GridStatusRaidDebuff:BossNameId(zoneid, 100, "Chronikar")
+GridStatusRaidDebuff:DebuffId(zoneid, 413105, 101, 5, 5, true) --Eon Shatter
+GridStatusRaidDebuff:DebuffId(zoneid, 403486, 102, 5, 5, true) --Eon Residue
+GridStatusRaidDebuff:DebuffId(zoneid, 403259, 103, 5, 5, true) --Residue Blast
+GridStatusRaidDebuff:DebuffId(zoneid, 405970, 104, 5, 5, true) --Eon Fragments
+GridStatusRaidDebuff:DebuffId(zoneid, 413013, 105, 5, 5, true) --Chronoshear
+GridStatusRaidDebuff:DebuffId(zoneid, 413041, 106, 5, 5, true) --Sheared Lifespan
+GridStatusRaidDebuff:DebuffId(zoneid, 401421, 107, 5, 5, true) --Sand Stomp
+GridStatusRaidDebuff:DebuffId(zoneid, 401794, 108, 5, 5, true) --Withering Sandpool
+
+GridStatusRaidDebuff:BossNameId(zoneid, 200, "Manifested Timeways")
+GridStatusRaidDebuff:DebuffId(zoneid, 403910, 201, 5, 5, true) --Decaying Time
+GridStatusRaidDebuff:DebuffId(zoneid, 403912, 202, 5, 5, true) --Accelerating Time
+GridStatusRaidDebuff:DebuffId(zoneid, 404141, 203, 5, 5, true) --Chrono-faded
+GridStatusRaidDebuff:DebuffId(zoneid, 405448, 204, 5, 5, true) --Chronofade
+GridStatusRaidDebuff:DebuffId(zoneid, 405431, 205, 5, 5, true) --Fragments of Time
+GridStatusRaidDebuff:DebuffId(zoneid, 414303, 206, 5, 5, true) --Unwind
+GridStatusRaidDebuff:DebuffId(zoneid, 414307, 207, 5, 5, true) --Radiant
+
+GridStatusRaidDebuff:BossNameId(zoneid, 300, "Blight of Galakrond")
+GridStatusRaidDebuff:DebuffId(zoneid, 406886, 301, 5, 5, true) --Corrosive Infusion
+GridStatusRaidDebuff:DebuffId(zoneid, 407406, 302, 5, 5, true) --Corrosion
+GridStatusRaidDebuff:DebuffId(zoneid, 418346, 303, 5, 5, true) --Corrupted Mind
+GridStatusRaidDebuff:DebuffId(zoneid, 407027, 304, 5, 5, true) --Corrosive Expulsion
+GridStatusRaidDebuff:DebuffId(zoneid, 407159, 305, 5, 5, true) --Blight Reclamation
+GridStatusRaidDebuff:DebuffId(zoneid, 407057, 306, 5, 5, true) --Blight Seep
+GridStatusRaidDebuff:DebuffId(zoneid, 407978, 307, 5, 5, true) --Necrotic Winds
+GridStatusRaidDebuff:DebuffId(zoneid, 413608, 308, 5, 5, true) --Essence Connection
+GridStatusRaidDebuff:DebuffId(zoneid, 408029, 309, 5, 5, true) --Necrofrost
+GridStatusRaidDebuff:DebuffId(zoneid, 408141, 310, 5, 5, true) --Incinerating Blightbreath
+GridStatusRaidDebuff:DebuffId(zoneid, 413590, 311, 5, 5, true) --Noxious Ejection
+
+GridStatusRaidDebuff:BossNameId(zoneid, 400, "Iridikron the Stonescaled")
+GridStatusRaidDebuff:DebuffId(zoneid, 409261, 401, 5, 5, true) --Extinction Blast
+GridStatusRaidDebuff:DebuffId(zoneid, 414330, 402, 5, 5, true) --Timeline Protection
+GridStatusRaidDebuff:DebuffId(zoneid, 414353, 403, 5, 5, true) --Exhausted
+GridStatusRaidDebuff:DebuffId(zoneid, 414496, 404, 5, 5, true) --Timeline Acceleration
+GridStatusRaidDebuff:DebuffId(zoneid, 414535, 405, 5, 5, true) --Stonecracker Barrage
+GridStatusRaidDebuff:DebuffId(zoneid, 414552, 406, 5, 5, true) --Stonecrack
+GridStatusRaidDebuff:DebuffId(zoneid, 409456, 407, 5, 5, true) --Earthsurge
+GridStatusRaidDebuff:DebuffId(zoneid, 409287, 408, 5, 5, true) --Rending Earthspikes
+GridStatusRaidDebuff:DebuffId(zoneid, 414376, 409, 5, 5, true) --Punctured Ground
+GridStatusRaidDebuff:DebuffId(zoneid, 409635, 410, 5, 5, true) --Pulverizing Exhalation
+GridStatusRaidDebuff:DebuffId(zoneid, 409884, 411, 5, 5, true) --Pulverizing Creations
+GridStatusRaidDebuff:DebuffId(zoneid, 416256, 412, 5, 5, true) --Stonebolt
+GridStatusRaidDebuff:DebuffId(zoneid, 409692, 413, 5, 5, true) --Patient Tactician
+GridStatusRaidDebuff:DebuffId(zoneid, 414184, 414, 5, 5, true) --Cataclysmic Obliteration
+GridStatusRaidDebuff:DebuffId(zoneid, 414293, 415, 5, 5, true) --Timeline Transcendence
+GridStatusRaidDebuff:DebuffId(zoneid, 414075, 416, 5, 5, true) --Crushing Onslaught
+
+GridStatusRaidDebuff:BossNameId(zoneid, 500, "Tyr, the Infinite Keeper")
+GridStatusRaidDebuff:DebuffId(zoneid, 404296, 501, 5, 5, true) --Infinite Hand Technique
+GridStatusRaidDebuff:DebuffId(zoneid, 410240, 502, 5, 5, true) --Titanic Blow
+GridStatusRaidDebuff:DebuffId(zoneid, 403724, 503, 5, 5, true) --Consecrated Ground
+GridStatusRaidDebuff:DebuffId(zoneid, 401463, 504, 5, 5, true) --Infinite Annihilation
+GridStatusRaidDebuff:DebuffId(zoneid, 400641, 505, 5, 5, true) --Dividing Strike
+GridStatusRaidDebuff:DebuffId(zoneid, 408183, 506, 5, 5, true) --Titanic Empowerment
+GridStatusRaidDebuff:DebuffId(zoneid, 400649, 507, 5, 5, true) --Spark of Tyr
+GridStatusRaidDebuff:DebuffId(zoneid, 408768, 508, 5, 5, true) --Siphon Oathstone
+GridStatusRaidDebuff:DebuffId(zoneid, 410249, 509, 5, 5, true) --Radiant Barrier
+GridStatusRaidDebuff:DebuffId(zoneid, 404315, 510, 5, 5, true) --Temporal Essence
+GridStatusRaidDebuff:DebuffId(zoneid, 406543, 511, 5, 5, true) --Stolen Time
+
+GridStatusRaidDebuff:BossNameId(zoneid, 600, "Morchie")
+GridStatusRaidDebuff:DebuffId(zoneid, 404916, 601, 5, 5, true) --Sand Blast
+GridStatusRaidDebuff:DebuffId(zoneid, 403891, 602, 5, 5, true) --More Problems!
+GridStatusRaidDebuff:DebuffId(zoneid, 404365, 603, 5, 5, true) --Dragon's Breath
+GridStatusRaidDebuff:DebuffId(zoneid, 413208, 604, 5, 5, true) --Sand Buffeted
+GridStatusRaidDebuff:DebuffId(zoneid, 405279, 605, 5, 5, true) --Familiar Faces
+GridStatusRaidDebuff:DebuffId(zoneid, 401197, 606, 5, 5, true) --Fixate
+GridStatusRaidDebuff:DebuffId(zoneid, 412768, 607, 5, 5, true) --Anachronistic Decay
+GridStatusRaidDebuff:DebuffId(zoneid, 406481, 608, 5, 5, true) --Time Traps
+GridStatusRaidDebuff:DebuffId(zoneid, 401667, 609, 5, 5, true) --Time Stasis
+GridStatusRaidDebuff:DebuffId(zoneid, 406100, 610, 5, 5, true) --Temporal Backlash
+
+GridStatusRaidDebuff:BossNameId(zoneid, 700, "Time-Lost Battlefield")
+GridStatusRaidDebuff:DebuffId(zoneid, 407120, 701, 5, 5, true) --Serrated Axe
+GridStatusRaidDebuff:DebuffId(zoneid, 406962, 702, 5, 5, true) --Axe Throw
+GridStatusRaidDebuff:DebuffId(zoneid, 407122, 703, 5, 5, true) --Rain of Fire
+GridStatusRaidDebuff:DebuffId(zoneid, 407121, 704, 5, 5, true) --Immolate
+GridStatusRaidDebuff:DebuffId(zoneid, 410234, 705, 5, 5, true) --Bladestorm
+GridStatusRaidDebuff:DebuffId(zoneid, 410254, 706, 5, 5, true) --Decapitate
+GridStatusRaidDebuff:DebuffId(zoneid, 410497, 707, 5, 5, true) --Mortal Wounds
+GridStatusRaidDebuff:DebuffId(zoneid, 419602, 708, 5, 5, true) --Thirst for Battle
+GridStatusRaidDebuff:DebuffId(zoneid, 408227, 709, 5, 5, true) --Shockwave
+GridStatusRaidDebuff:DebuffId(zoneid, 418046, 710, 5, 5, true) --FOR THE HORDE!
+GridStatusRaidDebuff:DebuffId(zoneid, 410496, 711, 5, 5, true) --War Cry
+
+GridStatusRaidDebuff:BossNameId(zoneid, 800, "Chrono-Lord Deios")
+GridStatusRaidDebuff:DebuffId(zoneid, 416152, 801, 5, 5, true) --Summon Infinite Keeper
+GridStatusRaidDebuff:DebuffId(zoneid, 416261, 802, 5, 5, true) --Collapsing Time Rift
+GridStatusRaidDebuff:DebuffId(zoneid, 412027, 803, 5, 5, true) --Chronal Burn
+GridStatusRaidDebuff:DebuffId(zoneid, 411763, 804, 5, 5, true) --Infinite Blast
+GridStatusRaidDebuff:DebuffId(zoneid, 410911, 805, 5, 5, true) --Time-Displaced Trooper
+GridStatusRaidDebuff:DebuffId(zoneid, 411023, 806, 5, 5, true) --Time-Displacement
+GridStatusRaidDebuff:DebuffId(zoneid, 410904, 807, 5, 5, true) --Infinity Orb
+GridStatusRaidDebuff:DebuffId(zoneid, 410908, 808, 5, 5, true) --Infinity Nova
+GridStatusRaidDebuff:DebuffId(zoneid, 416139, 809, 5, 5, true) --Temporal Breath
+GridStatusRaidDebuff:DebuffId(zoneid, 416264, 810, 5, 5, true) --Infinite Corruption
+GridStatusRaidDebuff:DebuffId(zoneid, 417413, 811, 5, 5, true) --Temporal Scar
