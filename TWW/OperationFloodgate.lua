c683008e86b57475aa9678226305db22de5d16ca
tbkizle <tbkizle@gmail.com>
2025-03-04 22:26:06 -0500

    [TWW\OperationFloodgate] Initial Support

---
+++ TWW/OperationFloodgate.lua
@@ -0,0 +1,47 @@
+-- local zone = "Operation: Floodgate"
+local zoneid = 2387
+
+-- "Big M.O.M.M.A."
+GridStatusRaidDebuff:BossNameId(zoneid, 100, "Big M.O.M.M.A.")
+GridStatusRaidDebuff:DebuffId(zoneid, 460156, 101, 5, 5, true) --Jumpstart
+GridStatusRaidDebuff:DebuffId(zoneid, 473287, 102, 5, 5, true) --Excessive Electrification
+GridStatusRaidDebuff:DebuffId(zoneid, 471585, 103, 5, 5, true) --Mobilize Mechadrones
+GridStatusRaidDebuff:DebuffId(zoneid, 1214780, 104, 5, 5, true) --Maximum Distortion
+GridStatusRaidDebuff:DebuffId(zoneid, 460393, 105, 5, 5, true) --Shoot
+GridStatusRaidDebuff:DebuffId(zoneid, 472452, 106, 5, 5, true) --Doom Storm
+GridStatusRaidDebuff:DebuffId(zoneid, 473351, 107, 5, 5, true) --Electrocrush
+GridStatusRaidDebuff:DebuffId(zoneid, 473220, 108, 5, 5, true) --Sonic Boom
+GridStatusRaidDebuff:DebuffId(zoneid, 469981, 109, 5, 5, true) --Kill-o-Block Barrier
+
+-- "Demolition Duo"
+GridStatusRaidDebuff:BossNameId(zoneid, 200, "Demolition Duo")
+GridStatusRaidDebuff:DebuffId(zoneid, 460867, 201, 5, 5, true) --Big Bada BOOM!
+GridStatusRaidDebuff:DebuffId(zoneid, 460787, 202, 5, 5, true) --Deflagration
+GridStatusRaidDebuff:DebuffId(zoneid, 472755, 203, 5, 5, true) --Shrapnel
+GridStatusRaidDebuff:DebuffId(zoneid, 1217653, 204, 5, 5, true) --B.B.B.F.G.
+GridStatusRaidDebuff:DebuffId(zoneid, 460602, 205, 5, 5, true) --Quick Shot
+GridStatusRaidDebuff:DebuffId(zoneid, 473690, 206, 5, 5, true) --Kinetic Explosive Gel
+GridStatusRaidDebuff:DebuffId(zoneid, 459779, 207, 5, 5, true) --Barreling Charge
+GridStatusRaidDebuff:DebuffId(zoneid, 459799, 208, 5, 5, true) --Wallop
+GridStatusRaidDebuff:DebuffId(zoneid, 470090, 209, 5, 5, true) --Divided Duo
+
+-- "Swampface"
+GridStatusRaidDebuff:BossNameId(zoneid, 300, "Swampface")
+GridStatusRaidDebuff:DebuffId(zoneid, 470039, 301, 5, 5, true) --Razorchoke Vines
+GridStatusRaidDebuff:DebuffId(zoneid, 473070, 302, 5, 5, true) --Awaken the Swamp
+GridStatusRaidDebuff:DebuffId(zoneid, 473047, 303, 5, 5, true) --Skewering Root
+GridStatusRaidDebuff:DebuffId(zoneid, 473052, 304, 5, 5, true) --Rushing Tide
+GridStatusRaidDebuff:DebuffId(zoneid, 473112, 305, 5, 5, true) --Mudslide
+GridStatusRaidDebuff:DebuffId(zoneid, 469478, 306, 5, 5, true) --Sludge Claws
+
+-- "Geezle Gigazap"
+GridStatusRaidDebuff:BossNameId(zoneid, 400, "Geezle Gigazap")
+GridStatusRaidDebuff:DebuffId(zoneid, 465456, 401, 5, 5, true) --Turbo Charge
+GridStatusRaidDebuff:DebuffId(zoneid, 466088, 402, 5, 5, true) --Turbo Bolt
+GridStatusRaidDebuff:DebuffId(zoneid, 468276, 403, 5, 5, true) --Dam!
+GridStatusRaidDebuff:DebuffId(zoneid, 468261, 404, 5, 5, true) --Dam Water
+GridStatusRaidDebuff:DebuffId(zoneid, 468661, 405, 5, 5, true) --Shock Water
+GridStatusRaidDebuff:DebuffId(zoneid, 468606, 406, 5, 5, true) --Dam Rubble
+GridStatusRaidDebuff:DebuffId(zoneid, 468846, 407, 5, 5, true) --Leaping Sparks
+GridStatusRaidDebuff:DebuffId(zoneid, 468812, 408, 5, 5, true) --Gigazap
+GridStatusRaidDebuff:DebuffId(zoneid, 466197, 409, 5, 5, true) --Thunder Punch
