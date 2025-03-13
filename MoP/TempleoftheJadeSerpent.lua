dde8baec285940fd2617f0b88e5398e168298e0a
tbkizle <tbkizle@gmail.com>
2025-03-13 15:51:20 -0400

    Update MoP\TempleoftheJadeSerpent

--- MoP/TempleoftheJadeSerpent.lua
+++ MoP/TempleoftheJadeSerpent.lua
@@ -20,17 +20,24 @@ _G.GridStatusRaidDebuff:DebuffId(zoneid, 128051, 1, 6, 6, true) --Serrated Slash
 _G.GridStatusRaidDebuff:DebuffId(zoneid, 114826, 1, 6, 6, true) --Songbird Serenade
 _G.GridStatusRaidDebuff:DebuffId(zoneid, 110099, 1, 6, 6, true) --Shadows of Doubt
 _G.GridStatusRaidDebuff:DebuffId(zoneid, 397911, 1, 6, 6, true) --Touch of Ruin
+_G.GridStatusRaidDebuff:DebuffId(zoneid, 397904, 1, 6, 6, true) --Setting Sun Kick
+_G.GridStatusRaidDebuff:DebuffId(zoneid, 397914, 1, 6, 6, true) --Defiling Mist
+_G.GridStatusRaidDebuff:DebuffId(zoneid, 397878, 1, 6, 6, true) --Tainted Ripple
 
 --Wise Mari
 _G.GridStatusRaidDebuff:BossNameId(zoneid, 10, "Wise Mari")
 _G.GridStatusRaidDebuff:DebuffId(zoneid, 115167, 11, 6, 6, true)
 _G.GridStatusRaidDebuff:DebuffId(zoneid, 143459, 12, 6, 6, true)
+_G.GridStatusRaidDebuff:DebuffId(zoneid, 396874, 13, 6, 6, true) --Water Bubble Burst
+
 --Lorewalker Stonestep
+
 --Liu Flameheart
 _G.GridStatusRaidDebuff:BossNameId(zoneid, 20, "Liu Flameheart")
 _G.GridStatusRaidDebuff:DebuffId(zoneid, 106823, 21, 6, 6, true)
 _G.GridStatusRaidDebuff:DebuffId(zoneid, 106841, 22, 6, 6, true)
 _G.GridStatusRaidDebuff:DebuffId(zoneid, 107045, 23, 6, 6, true)
+
 --Sha of Doubt
 _G.GridStatusRaidDebuff:BossNameId(zoneid, 30, "Sha of Doubt")
 _G.GridStatusRaidDebuff:DebuffId(zoneid, 106113, 31, 6, 6, true)
