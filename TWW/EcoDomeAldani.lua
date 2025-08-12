e3764332c7176c970131347eded7a4e2fcc146fb
tbkizle <tbkizle@gmail.com>
2025-08-12 01:44:58 -0400

    Initial Support for Eco-Dome Al'dani

---
+++ TWW/EcoDomeAldani.lua
@@ -0,0 +1,50 @@
+-- local zone = "Ara-Kara, City of Echoes"
+local zoneid = 2357
+
+--Trash
+
+
+--M+
+GridStatusRaidDebuff:DebuffId(zoneid, 240447, 1, 6, 6, true) --Quake
+GridStatusRaidDebuff:DebuffId(zoneid, 240559, 1, 6, 6, true, true) --Grievous Wound
+GridStatusRaidDebuff:DebuffId(zoneid, 209858, 1, 6, 6, true, true) --Necrotic Wound
+GridStatusRaidDebuff:DebuffId(zoneid, 240443, 1, 6, 6, true, true) --Burst
+GridStatusRaidDebuff:DebuffId(zoneid, 226512, 1, 6, 6, true) --Sanguine Ichor
+GridStatusRaidDebuff:DebuffId(zoneid, 409492, 1, 6, 6, true, true) --Afflicted Cry
+GridStatusRaidDebuff:DebuffId(zoneid, 408805, 1, 6, 6, true) --Destabilize
+GridStatusRaidDebuff:DebuffId(zoneid, 408556, 1, 6, 6, true) --Entangled
+GridStatusRaidDebuff:DebuffId(zoneid, 440313, 1, 6, 6, true) --Devouring Rift
+
+--Azhiccar
+GridStatusRaidDebuff:BossNameId(zoneid, 100, "Azhiccar")
+GridStatusRaidDebuff:DebuffId(zoneid, 1217232, 101, 5, 5, true) --Devour
+GridStatusRaidDebuff:DebuffId(zoneid, 1217241, 102, 5, 5, true) --Feast
+GridStatusRaidDebuff:DebuffId(zoneid, 1217327, 103, 5, 5, true) --Invading Shriek
+GridStatusRaidDebuff:DebuffId(zoneid, 1231811, 104, 5, 5, true) --Uncontrolled
+GridStatusRaidDebuff:DebuffId(zoneid, 1217381, 105, 5, 5, true) --Engorge
+GridStatusRaidDebuff:DebuffId(zoneid, 1217436, 106, 5, 5, true) --Toxic Regurgitation
+GridStatusRaidDebuff:DebuffId(zoneid, 1217446, 107, 5, 5, true) --Digestive Spittle
+GridStatusRaidDebuff:DebuffId(zoneid, 1217664, 108, 5, 5, true) --Thrash
+
+--Taah'bat and A'wazj
+GridStatusRaidDebuff:BossNameId(zoneid, 200, "Taah'bat and A'wazj")
+GridStatusRaidDebuff:DebuffId(zoneid, 1219417, 201, 5, 5, true) --Beastmaster's Bond
+GridStatusRaidDebuff:DebuffId(zoneid, 1219700, 202, 5, 5, true) --Arcane Blitz
+GridStatusRaidDebuff:DebuffId(zoneid, 1219457, 203, 5, 5, true) --Incorporeal
+GridStatusRaidDebuff:DebuffId(zoneid, 1219731, 204, 5, 5, true) --Destabilized
+GridStatusRaidDebuff:DebuffId(zoneid, 1220497, 205, 5, 5, true) --Arcane Overload
+GridStatusRaidDebuff:DebuffId(zoneid, 1227137, 206, 5, 5, true) --Warp Strike
+GridStatusRaidDebuff:DebuffId(zoneid, 1219536, 207, 5, 5, true) --Binding Javelin
+GridStatusRaidDebuff:DebuffId(zoneid, 1219482, 208, 5, 5, true) --Rift Claws
+
+--Soul-Scribe
+GridStatusRaidDebuff:BossNameId(zoneid, 300, "Soul-Scribe")
+GridStatusRaidDebuff:DebuffId(zoneid, 1224793, 301, 5, 5, true) --Whispers of Fate
+GridStatusRaidDebuff:DebuffId(zoneid, 1224865, 302, 5, 5, true) --Fatebound
+GridStatusRaidDebuff:DebuffId(zoneid, 1236703, 303, 5, 5, true) --Eternal Weave
+GridStatusRaidDebuff:DebuffId(zoneid, 1237184, 304, 5, 5, true) --Splinters of Fate
+GridStatusRaidDebuff:DebuffId(zoneid, 1225218, 305, 5, 5, true) --Dread of the Unknown
+GridStatusRaidDebuff:DebuffId(zoneid, 1226444, 306, 5, 5, true) --Wounded Fate
+GridStatusRaidDebuff:DebuffId(zoneid, 1225162, 307, 5, 5, true) --Ceremonial Dagger
+GridStatusRaidDebuff:DebuffId(zoneid, 1242000, 308, 5, 5, true) --Echoes of Fate
+
